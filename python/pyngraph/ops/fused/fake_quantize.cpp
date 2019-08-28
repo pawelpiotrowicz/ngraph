a8e7d01324600166900ab19f63143e7d9929b221
aslepko <44713115+aslepko@users.noreply.github.com>
2019-08-27 22:34:14 -0700

    Update fake_quantize.cpp

--- python/pyngraph/ops/fused/fake_quantize.cpp
+++ python/pyngraph/ops/fused/fake_quantize.cpp
@@ -32,5 +32,5 @@ void regclass_pyngraph_op_FakeQuantize(py::module m)
                               const std::shared_ptr<ngraph::Node>&,
                               const std::shared_ptr<ngraph::Node>&,
                               const std::shared_ptr<ngraph::Node>&,
-                              unsigned int&>());
+                              unsigned long&>());
 }
